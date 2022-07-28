2ca7e128c2ac5a23b472fff2910f75f10d037982
Jordan96L <jordan-ling@hotmail.co.uk>
2022-07-28 14:38:39 +0100

    add in sort by option

---
+++ src/components/SortBy.jsx
@@ -0,0 +1,29 @@
+export default function SortBy({ setSortColumn, setSortOrder }) {
+    return (
+      <div>
+        {/* <h2>Sort by</h2> */}
+        <form>
+          <label htmlFor="sortBy">Sort by:</label>
+          <select
+            id="sortBy"
+            onChange={(e) => {
+              setSortColumn(e.target.value);
+            }}
+          >
+            <option value={"created_at"}>date</option>
+            <option value={"votes"}>votes</option>
+          </select>
+          <select
+            id="order"
+            onChange={(e) => {
+              setSortOrder(e.target.value);
+            }}
+          >
+            <option value={"desc"}>descending</option>
+            <option value={"asc"}>ascending</option>
+          </select>
+          <br />
+        </form>
+      </div>
+    );
+  }
